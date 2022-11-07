6e27d502cc98f04bc96084c40e5a891d1e6db1f4
vcpopa <81989171+vcpopa@users.noreply.github.com>
2022-11-07 15:22:03 +0000

    Update causal_impact_app.py

--- causal_impact_app.py
+++ causal_impact_app.py
@@ -67,7 +67,10 @@ class Impact():
         
         pre_period = [pd.to_datetime(date) for date in  [self.df[self.date_col].min(),self.df[self.date_col].iloc[self.treatment_start_index-1]]]
         st.markdown("PRE-PERIOD")
+        st.markdown(pre_period)
+        st.markdown("POST-PERIOD")
         post_period = [pd.to_datetime(date) for date in [self.df[self.date_col].iloc[self.treatment_start_index],self.df[self.date_col].max()]]
+        st.markdown(post_period)
         self.df.set_index(self.date_col,inplace=True)
 
 
