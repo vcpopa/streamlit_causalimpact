659e909d7f373421a1b30755860a7ce7f4c6b025
vcpopa <81989171+vcpopa@users.noreply.github.com>
2022-11-07 15:08:11 +0000

    Update causal_impact_app.py

--- causal_impact_app.py
+++ causal_impact_app.py
@@ -96,34 +96,34 @@ if __name__=="__main__":
         
         if file is not None:
             df=pd.read_csv(file)
-        df2=df.copy()
-        cols_menu_opts=df.columns
-        mask = df2.astype(str).apply(lambda x : x.str.match('(\d{2,4}(-|\/|\\|\.| )\d{2}(-|\/|\\|\.| )\d{2,4})+').any())
-        date_column_options=df2.loc[:,mask].columns
-        date_col= st.sidebar.selectbox("Select date column", date_column_options)
-        df[date_col]=pd.to_datetime(df[date_col])
-        control= st.sidebar.selectbox("Select control column", cols_menu_opts)
-        response= st.sidebar.selectbox("Select response column", cols_menu_opts)
-        treatment_start=st.sidebar.date_input(label="Enter your intervention start date",value=None,min_value=df[date_col].min(),max_value=None)
+            df2=df.copy()
+            cols_menu_opts=df.columns
+            mask = df2.astype(str).apply(lambda x : x.str.match('(\d{2,4}(-|\/|\\|\.| )\d{2}(-|\/|\\|\.| )\d{2,4})+').any())
+            date_column_options=df2.loc[:,mask].columns
+            date_col= st.sidebar.selectbox("Select date column", date_column_options)
+            df[date_col]=pd.to_datetime(df[date_col])
+            control= st.sidebar.selectbox("Select control column", cols_menu_opts)
+            response= st.sidebar.selectbox("Select response column", cols_menu_opts)
+            treatment_start=st.sidebar.date_input(label="Enter your intervention start date",value=None,min_value=df[date_col].min(),max_value=None)
 
-        if st.sidebar.button("RUN"):
-            imp=Impact(df=df,date_col=date_col,control=control,response=response,treatment_start=treatment_start)
-            ts_fig,impact_plot,impact_summary,report=imp.causal_impact()
+            if st.sidebar.button("RUN"):
+                imp=Impact(df=df,date_col=date_col,control=control,response=response,treatment_start=treatment_start)
+                ts_fig,impact_plot,impact_summary,report=imp.causal_impact()
 
-            st.pyplot(ts_fig)
+                st.pyplot(ts_fig)
 
-            st.header("IMPACT PLOTS")
-            st.pyplot(impact_plot)
+                st.header("IMPACT PLOTS")
+                st.pyplot(impact_plot)
 
-            st.header("SUMMARY REPORT")
-            impact_summary=impact_summary.replace("\n","<br>")
-            impact_summary=impact_summary.replace("{Causal Impact}","")
-            impact_summary=impact_summary.replace("For more details run the command: print(impact.summary('report'))","")
-            st.markdown(impact_summary,unsafe_allow_html=True)
+                st.header("SUMMARY REPORT")
+                impact_summary=impact_summary.replace("\n","<br>")
+                impact_summary=impact_summary.replace("{Causal Impact}","")
+                impact_summary=impact_summary.replace("For more details run the command: print(impact.summary('report'))","")
+                st.markdown(impact_summary,unsafe_allow_html=True)
 
-            st.header("FULL REPORT")
-            report=report.replace("{CausalImpact}","")
-            st.markdown(report)
+                st.header("FULL REPORT")
+                report=report.replace("{CausalImpact}","")
+                st.markdown(report)
     
     except:
         st.markdown(traceback.format_exc())
